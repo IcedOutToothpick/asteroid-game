6fd4a0900fda4b6f74ba6b5687ce560df21bd839
IcedOutToothpick <robert.steele27@protonmail.com>
2025-10-25 14:27:39 -0400

    base version of game finished

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -21,7 +36,20 @@ def main():
                 return
         screen.fill("black")
 
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.is_colliding_with(player):
+                print("Game over!")
+                return
+            
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.is_colliding_with(shot):
+                    asteroid.split()
+                    shot.kill()
         
         pygame.display.flip()
         dt = clock.tick(60) / 1000
